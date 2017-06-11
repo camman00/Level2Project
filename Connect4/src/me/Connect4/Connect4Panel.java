3366f600649adbe5e503371d553f299fbef4a65c
camman00 <league@WorthyMini-5.attlocal.net>
2017-06-11 12:56:11 -0700

    Finally found my password it was joeyrocks67
    My password was changed to samplemusic1982

--- Connect4/src/me/Connect4/Connect4Panel.java
+++ Connect4/src/me/Connect4/Connect4Panel.java
@@ -11,10 +11,12 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class Connect4Panel extends JPanel {
 	private BufferedImage Connect4PanelImage;
+	private BufferedImage test;
 
 	public Connect4Panel() {
 		try {
 			Connect4PanelImage = ImageIO.read(new File("src/Connect4Board.png"));
+			test = ImageIO.read(new File("src/redpawn.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -23,15 +25,18 @@ public class Connect4Panel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.drawImage(Connect4PanelImage, 0, 0, this);
+		g.drawImage(test, (640 / 7) - 70 - 6, 6, this);
+		//640/7 = The lenght -70 is compensation and -6 is a graphic porporiton fix
 		repaint();
 	}
 
 	public int getWidth() {
-		return Connect4PanelImage.getWidth();
+		return 640;
 	}
 
 	public int getHeight() {
-		return Connect4PanelImage.getHeight();
+		return 480;
 	}
+	
 
 }
--- Connect4/src/me/Connect4/Pawn/PawnHolder.java
+++ Connect4/src/me/Connect4/Pawn/PawnHolder.java
@@ -1,5 +1,32 @@
 package me.Connect4.Pawn;
 
 public class PawnHolder {
-
+	private Pawn pawn;
+	private int rowVertical;
+	private int rowHorizontal;
+	public static PawnHolder[] PawnHolderList = new PawnHolder[43];
+	public PawnHolder(Pawn pawn,int rowVertical,int rowHorizontal) {
+		this.setPawn(pawn);
+		this.setRowVertical(rowVertical);
+		this.setRowHorizontal(rowHorizontal);
+		PawnHolderList[PawnHolderList.length] = this;
+	}
+	public Pawn getPawn() {
+		return pawn;
+	}
+	public void setPawn(Pawn pawn) {
+		this.pawn = pawn;
+	}
+	public int getRowVertical() {
+		return rowVertical;
+	}
+	public void setRowVertical(int rowVertical) {
+		this.rowVertical = rowVertical;
+	}
+	public int getRowHorizontal() {
+		return rowHorizontal;
+	}
+	public void setRowHorizontal(int rowHorizontal) {
+		this.rowHorizontal = rowHorizontal;
+	}
 }
